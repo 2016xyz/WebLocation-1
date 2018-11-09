f2acc5b6ea504edc250115af2e4adb041b4a72d7
Baoanony <Baoanony@163.com>
2018-11-09 20:15:04 +0800

    修改了link-sql文件

--- php/sql/link-sql.php
+++ php/sql/link-sql.php
@@ -1,14 +1,26 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    
-   //创建连接
-   $conn = new mysqli($servername, $username, $password);
-   
-   //检测连接
-   if ($conn->connect_error) {
-       die("连接失败:" . $conn->connect_error);
-   }
-   echo"连接成功";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+ 
+// 创建连接
+$conn = new mysqli($servername, $username, $password, $dbname);
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+}else{
+  echo "链接成功";
+}
+
+/*
+// 创建数据库
+$sql = "CREATE DATABASE myDB";
+if (mysqli_query($conn, $sql)) {
+    echo "数据库创建成功";
+} else {
+    echo "Error creating database: " . mysqli_error($conn);
+}
+ 
+mysqli_close($conn);*/
 ?>

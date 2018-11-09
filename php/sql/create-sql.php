daa844404b12051626fc6daf53bc5b9d6c732bbf
Baoanony <Baoanony@163.com>
2018-11-09 21:43:03 +0800

    把多个数据库合并成一个文件

--- php/sql/create-sql.php
+++ php/sql/create-sql.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "myDB";
+$server = "localhost";
+$user = "root";
+$passwd = "";
+$datebase = "UserData";
  
 // 创建连接
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($server, $user, $passwd, $datebase);
 // 检测连接
 if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
@@ -13,7 +13,7 @@ if ($conn->connect_error) {
   echo "链接成功";
 }
 
-
+/*
 // 创建数据库
 $sql = "CREATE DATABASE UserData";
 if (mysqli_query($conn, $sql)) {
@@ -21,6 +21,22 @@ if (mysqli_query($conn, $sql)) {
 } else {
     echo "Error creating database: " . mysqli_error($conn);
 }
+*/
+//--------------------------------------------------------
  
-mysqli_close($conn);
+ $sql = "CREATE TABLE UserData (
+    id INT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    username VARCHAR(30) NOT NULL,
+    userpasswd VARCHAR(30) NOT NULL,
+    jd DOUBLE(6,6) NOT NULL,
+    wd DOUBLE(6,6) NOT NULL
+)";
+ 
+if ($conn->query($sql) === TRUE) {
+    echo "创建成功";
+} else {
+    echo "创建数据表错误: " . $conn->error;
+}
+ 
+$conn->close();
 ?>
